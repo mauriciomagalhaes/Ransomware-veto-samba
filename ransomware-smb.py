7c0b06763b623044a31589333da35ce9028f12df
mauriciomagalhaes <contato@mauriciomagalhaes.com.br>
2019-09-09 13:32:43 -0300

    Versão 2.0

--- ransomware-smb.py
+++ ransomware-smb.py
@@ -2,23 +2,26 @@
 # -*- coding: UTF-8 -*-
 import requests
 import json
-import sys
+import sys, pprint
+import datetime, time, dateutil.parser
 
 try:
     req = requests.get('https://fsrm.experiant.ca/api/v1/combined')
 except:
-    print('Nao pode acessar')
+    print('Problemas com acesso a página')
     exit()
 
 dic = json.loads(req.text)
+registros = (dic['api']['file_group_count'])
+lastupdate = (dic['lastUpdated'])
+lastupdate = dateutil.parser.parse(lastupdate).strftime('%d/%m/%Y - %H:%m')
+print('Número de Ransomwares: '+str(registros)+'\nUltimo update: '+str(lastupdate))
+
 rans = (dic['filters'])
-
-sys.stdout=open("ranswares.conf","w")
-
+sys.stdout=open("ransomwares.conf","w")
 print("veto files = ", end="")
-#for x in rans[:10]:
+
 for x in rans:
     print('/'+x, end="")
 
-sys.stdout.close()
-#sed 's/.//' | sed ':a;N;s/\n//g;ta'                                            
+sys.stdout.close()                                           
