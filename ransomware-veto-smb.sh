93915156d6084b5a7d1694eee806aad8725c5c62
mauriciomagalhaes <contato@mauriciomagalhaes.com.br>
2019-09-10 09:08:43 -0300

    Versão BASH

---
+++ ransomware-veto-smb.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# instale o comando jq 
+
+SMBCONF="/etc/samba"
+SMBCONTROL=$(which smbcontrol)
+JQ=$(which jq)
+TOTALREG=$(jq -r .api.file_group_count $SMBCONF/ransomwares.json)
+DATA=$(jq -r .lastUpdated $SMBCONF/ransomwares.json)
+
+
+while true; do
+    if curl --output /dev/null --silent --head --fail https://fsrm.experiant.ca/api/v1/combined; then
+        curl -o /etc/samba/ransomwares.json https://fsrm.experiant.ca/api/v1/combined && break
+    fi
+done
+
+echo "Total de Ransomwares conhecidos: $TOTALREG"
+echo "Ultima Atualização: $DATA"
+
+$JQ -r .filters[] $SMBCONF/ransomwares.json > $SMBCONF/ransomwares.conf
+
+sed -i 's/^/\//g' $SMBCONF/ransomwares.conf
+sed -i ':a;N;s/\n//g;ta' $SMBCONF/ransomwares.conf
+sed -i 's/^/veto files = /g' $SMBCONF/ransomwares.conf
+
+$SMBCONTROL smbd reload-config
+#$SMBCONTROL/smbcontrol nmbd reload-config
