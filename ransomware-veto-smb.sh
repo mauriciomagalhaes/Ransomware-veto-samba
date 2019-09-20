b7dea145ec6d0a5804ce841e7b928d263c759471
mauriciomagalhaes <contato@mauriciomagalhaes.com.br>
2019-09-20 10:00:42 -0300

    .

--- ransomware-veto-smb.sh
+++ ransomware-veto-smb.sh
@@ -5,14 +5,16 @@
 SMBCONF="/etc/samba"
 SMBCONTROL=$(which smbcontrol)
 JQ=$(which jq)
-TOTALREG=$(jq -r .api.file_group_count $SMBCONF/ransomwares.json)
-DATA=$(jq -r .lastUpdated $SMBCONF/ransomwares.json)
+
 
 while true; do
     if curl --output /dev/null --silent --head --fail https://fsrm.experiant.ca/api/v1/combined; then
-        curl -o /etc/samba/ransomwares.json https://fsrm.experiant.ca/api/v1/combined && break
+        curl -o $SMBCONF/ransomwares.json https://fsrm.experiant.ca/api/v1/combined && break
     fi
 done
+
+TOTALREG=$(jq -r .api.file_group_count $SMBCONF/ransomwares.json)
+DATA=$(jq -r .lastUpdated $SMBCONF/ransomwares.json)
 
 echo "Total de Ransomwares conhecidos: $TOTALREG"
 echo "Ultima Atualização: $DATA"
